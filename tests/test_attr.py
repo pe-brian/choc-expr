c240b409817ffe75ba9270206727af971db5f554
PEB 8 <pebrian@outlook.fr>
2024-05-03 10:14:15 +0200

    Attr tests restructured

--- tests/test_attr.py
+++ tests/test_attr.py
@@ -1,17 +1,29 @@
 from choc_expr import build_attr as build
 
 
-def test_attr_attr():
+def test_attr_build_simple_evaluation():
     assert build("val", {"val": 1}) == "1"
-    assert build("$(items)", {"items": (1, 2, 3)}) == "1, 2, 3"
-    assert build("$(items).b.c.val", {"items": ({"b": {"c": {"val": 42}}}, {"b": {"c": {"val": 43}}}, {"b": {"c": {"val": 44}}})}) == "42, 43, 44"
-    assert build("$(box.items).b.c.val", {"box": {"items": ({"b": {"c": {"val": 42}}}, {"b": {"c": {"val": 43}}}, {"b": {"c": {"val": 44}}})}}) == "42, 43, 44"
-    assert build("$(box.items).b.c.val~", {"box": {"items": ({"b": {"c": {"val": 42}}}, {"b": {"c": {"val": 43}}}, {"b": {"c": {"val": 44}}})}}) == "42, 43, 44\n"
 
 
-def test_attr_attr_endline():
+def test_attr_build_simple_evaluation_and_endline():
     assert build("val~", {"val": 1}) == "1\n"
 
 
-def test_attr_attr_empty_endline():
+def test_attr_build_empty_simple_evaluation_and_endline():
     assert build("val~", {"val": ""}) == ""
+
+
+def test_attr_build_iterable_evaluation():
+    assert build("$(items)", {"items": (1, 2, 3)}) == "1, 2, 3"
+
+
+def test_attr_build_iterable_evaluation_and_nested_attributes():
+    assert build("$(items).b.c.val", {"items": ({"b": {"c": {"val": 42}}}, {"b": {"c": {"val": 43}}}, {"b": {"c": {"val": 44}}})}) == "42, 43, 44"
+
+
+def test_attr_build_nested_iterable_evaluation_and_nested_attributes():
+    assert build("$(box.items).b.c.val", {"box": {"items": ({"b": {"c": {"val": 42}}}, {"b": {"c": {"val": 43}}}, {"b": {"c": {"val": 44}}})}}) == "42, 43, 44"
+
+
+def test_attr_build_nested_iterable_evaluation_and_nested_attributes_and_endline():
+    assert build("$(box.items).b.c.val~", {"box": {"items": ({"b": {"c": {"val": 42}}}, {"b": {"c": {"val": 43}}}, {"b": {"c": {"val": 44}}})}}) == "42, 43, 44\n"
